1b393097916310bbd05d5908211b8c442601cd68
jasurasqarov04-cell <jasurasqarov04@gmail.com>
2026-01-13 19:52:13 +0500

    Add Settings dataclass for configuration management

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
 import os
 from dataclasses import dataclass
+@dataclass
+class Settings:
+bot_token: str = os.getenv("BOT_TOKEN")
+ovce_ch_id: int = int(os.getenv("OVCE_CHANNEL_ID") or 0)
+admin_tg_id: int = int(os.getenv("ADMIN_TG_ID") or 0)
+db_url: str = "sqlite+aiosqlite:///./skot.db"
+cfg = Settings()
